3f741e5d717f877fa6e307a08faceaaba4e15722
tasooshi <tasooshi@pm.me>
2021-04-27 00:15:17 +0200

    Logging, C# conventions

--- Service1.cs
+++ Service1.cs
@@ -1,25 +1,19 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Management;
-using System.Runtime.CompilerServices;
 using System.ServiceProcess;
-using System.Text;
 using System.Xml;
 
 namespace ServiceWrapper
 {
     public partial class ServiceWrapper : ServiceBase
     {
-        private string serviceLogName;
-        private string serviceLogSource;
-        private XmlNode commands;
-        private ArrayList childProcesses = new ArrayList();
+        private string ServiceLogName;  
+        private string ServiceLogSource;
+        private XmlNode Commands;
+        private ArrayList ChildProcesses = new ArrayList();
 
         public ServiceWrapper()
         {
@@ -28,40 +22,23 @@ namespace ServiceWrapper
             configuration.Load(Path.Combine(executablePath, "ServiceWrapper.xml"));
             string serviceName = configuration.SelectSingleNode("//Name").InnerText;
             XmlNode logging = configuration.SelectSingleNode("//Logging");
-            serviceLogName = logging.SelectSingleNode("ServiceLogName").InnerText;
-            serviceLogSource = logging.SelectSingleNode("ServiceLogSource").InnerText;
-            commands = configuration.SelectSingleNode("//Commands");
-
+            ServiceLogName = logging.SelectSingleNode("ServiceLogName").InnerText;
+            ServiceLogSource = logging.SelectSingleNode("ServiceLogSource").InnerText;
+            Commands = configuration.SelectSingleNode("//Commands");
             InitializeComponent(serviceName);
-
-            if (!System.Diagnostics.EventLog.SourceExists(serviceLogSource))
+            if (!System.Diagnostics.EventLog.SourceExists(ServiceLogSource))
             {
-                System.Diagnostics.EventLog.CreateEventSource(serviceLogSource, serviceLogName);
+                System.Diagnostics.EventLog.CreateEventSource(ServiceLogSource, ServiceLogName);
             }
-            eventLog1.Source = serviceLogSource;
-            eventLog1.Log = serviceLogName;
-
-            foreach (XmlNode command in commands.ChildNodes)
-            {
-                if (Convert.ToBoolean(command.Attributes["enabled"].Value))
-                {
-                    Process proc = new Process();
-                    proc.StartInfo.FileName = command.SelectSingleNode("Executable").InnerText;
-                    proc.StartInfo.Arguments = command.SelectSingleNode("Arguments").InnerText;
-                    proc.Start();
-                    if (proc != null)
-                    {
-                        childProcesses.Add(proc.Id);
-                    }
-                }
-            }
+            eventLog1.Source = ServiceLogSource;
+            eventLog1.Log = ServiceLogName;
         }
 
         private static void KillProcessTree(int procId)
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher($"Select * From Win32_Process Where ParentProcessID={procId}");
-            ManagementObjectCollection moc = searcher.Get();
-            foreach (ManagementObject mo in moc)
+            ManagementObjectSearcher objectSearcher = new ManagementObjectSearcher($"Select * From Win32_Process Where ParentProcessID={procId}");
+            ManagementObjectCollection objectCollection = objectSearcher.Get();
+            foreach (ManagementObject mo in objectCollection)
             {
                 KillProcessTree(Convert.ToInt32(mo["ProcessID"]));
             }
@@ -72,6 +49,26 @@ namespace ServiceWrapper
         protected override void OnStart(string[] args)
         {
             eventLog1.WriteEntry($"{ServiceName} started");
+            foreach (XmlNode command in Commands.ChildNodes)
+            {
+                if (Convert.ToBoolean(command.Attributes["enabled"].Value))
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = command.SelectSingleNode("Executable").InnerText;
+                    proc.StartInfo.Arguments = command.SelectSingleNode("Arguments").InnerText;
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    if (proc.Start())
+                    {
+                        ChildProcesses.Add(proc.Id);
+                        eventLog1.WriteEntry($"Process {proc.Id} running");
+                    } else
+                    {
+                        string output = proc.StandardOutput.ReadToEnd();
+                        eventLog1.WriteEntry(output);
+                    }
+                }
+            }
         }
 
         protected override void OnShutdown()
@@ -81,30 +78,24 @@ namespace ServiceWrapper
 
         protected override void OnStop()
         {
-            foreach (int procId in childProcesses)
+            foreach (int procId in ChildProcesses)
             {
                 try
                 {
                     // Unfortunately due to .NET compatibility issues Process.Kill(true) can't be used
                     KillProcessTree(procId);
                 }
+                catch (System.ArgumentException)
+                {
+                    eventLog1.WriteEntry($"Process {procId} not running");
+                }
                 catch (Exception exc)
                 {
                     eventLog1.WriteEntry($"Failed killing {procId}: {exc}");
                 }
             }
-            childProcesses.Clear();
+            ChildProcesses.Clear();
             eventLog1.WriteEntry($"{ServiceName} stopped");
-        }
-
-        protected override void OnPause()
-        {
-            eventLog1.WriteEntry($"{ServiceName} paused");
-        }
-
-        protected override void OnContinue()
-        {
-            eventLog1.WriteEntry($"{ServiceName} resumed");
         }
 
     }
